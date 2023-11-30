1d14299c76ee94c2ce5a27aa2bbc4981c8c37bb8
cieffelle <lorenzo.cicuttin@recrowd.com>
2023-11-30 11:29:28 +0100

    expose args

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # brow
 
 A scripting language to automate chromium
+
+```
+$ git clone https://github.com/9elt/brow
+$ cd brow
+$ bun i
+```
+
+close all chromium windows, then run
+```
+$ chromium --remote-debugging-port=21222
+```
+
+then
+```
+$ bun ./index.js ./example 'elder scrolls sigma knight meme'
+```
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import puppeteer, { Browser, Page } from "puppeteer";
 
-const DEBUG = true;
+const DEBUG = false;
 
 /** @type {Browser} */
 let browser;
@@ -15,150 +15,155 @@ catch {
     process.exit(1);
 }
 
-for (const arg of Bun.argv.slice(2))
-    await main(arg);
+let path;
+try {
+    path = Bun.argv[2];
+}
+catch {
+    console.log('provide a file path');
+    process.exit(1);
+}
+
+let file;
+try {
+    file = await Bun.file(path).text();
+}
+catch {
+    console.log('provide a valid script');
+    process.exit(1);
+}
+
+const ARGS = Bun.argv.slice(2)
+    .reduce((c, arg, i) => (Object.assign(c, { ['$' + i]: arg })), {});
+
+/** @type {Page} */
+let page = 0;
+for (const p of await browser.pages())
+    if (await p.evaluate(() => document.visibilityState == 'visible')) {
+        page = p;
+        break;
+    }
+if (!page)
+    page = await browser.newPage();
+
+const COMMANDS = {
+    async goto(url) {
+        await page.goto(url);
+    },
+    async click(selector) {
+        if (await this.wait(selector))
+            await page.click(selector);
+    },
+    async focus(selector) {
+        if (await this.wait(selector))
+            await page.focus(selector);
+    },
+    async hover(selector) {
+        if (await this.wait(selector))
+            await page.hover(selector);
+    },
+    async select(selector) {
+        if (await this.wait(selector))
+            await page.select(selector);
+    },
+    async tap(selector) {
+        if (await this.wait(selector))
+            await page.tap(selector);
+    },
+    async wait(selector) {
+        try {
+            await page.waitForSelector(selector);
+            return true;
+        } catch {
+            return false;
+        }
+    },
+    async scroll(selector) {
+        await page.evaluate((selector) => {
+            const el = document.querySelector(selector);
+            el && el.scrollIntoView({ behavior: 'smooth' });
+        }, selector);
+    },
+    async sleep(ms) {
+        ms = parseInt(ms);
+        ms && await Bun.sleep(ms)
+    },
+    async press(key) {
+        await page.keyboard.press(key);
+    },
+    async type(text) {
+        if (await this.wait('*:focus'))
+            await page.type('*:focus', text);
+    },
+    async browser(js) {
+        return await page.evaluate((r) => eval(r), js);
+    },
+    async server(js) {
+        return eval(js);
+    },
+    async save(string) {
+        const [filename, ...data] = string.split(' ');
+        await Bun.write(filename, data.join(' '));
+        return true;
+    },
+    async yesNO() {
+        console.write('[y/N] ');
+        for await (const _ of console)
+            return /y(es)?/.test(_.trim().toLowerCase()) ? 1 : this.exit(1);
+    },
+    async YESno() {
+        console.write('[Y/n] ');
+        for await (const _ of console)
+            return /n(o)?/.test(_.trim().toLowerCase()) ? this.exit(1) : 1;
+    },
+    async log() { }
+};
+
+function parse_cmd(line) {
+    let char = '';
+    let i = 0;
+    let cmd = '';
+    let args = '';
+    while ((char = line.charAt(i++)) && char !== ' ')
+        cmd += char;
+    while ((char = line.charAt(i++)))
+        args += char;
+    for (const key in ARGS)
+        args = args.replace(key, ARGS[key]);
+    return [cmd, args];
+}
+
+await parse(file, async (line) => {
+    if (!line || line.startsWith('#'))
+        return;
+
+    await COMMANDS.sleep(150);
+
+    if (line.startsWith('$')) {
+        const [name, _] = parse_cmd(line);
+        const [command, args] = parse_cmd(_);
+        try {
+            ARGS[name] = command in COMMANDS
+                ? await COMMANDS[command](args) : _;
+        }
+        catch { }
+        return;
+    }
+
+    const [command, args] = parse_cmd(line);
+    console.write(esc(command, 38, 5, 244, 1), ' ', args, ' ');
+
+    try {
+        await COMMANDS[command](args);
+        console.write(esc('OK', 38, 5, 155, 1), '\n');
+    }
+    catch (err) {
+        console.write(esc('ERR', 38, 5, 203, 1), '\n');
+        DEBUG && console.log(err);
+    }
+});
 
 process.exit(0);
-
-async function main(path) {
-    let file;
-    try {
-        file = await Bun.file(path).text();
-    }
-    catch {
-        console.log('provide a valid script');
-        return 1;
-    }
-
-    /** @type {Page} */
-    let page = 0;
-    for (const p of await browser.pages())
-        if (await p.evaluate(() => document.visibilityState == 'visible')) {
-            page = p;
-            break;
-        }
-    if (!page)
-        page = await browser.newPage();
-
-    const COMMANDS = {
-        async goto(url) {
-            await page.goto(url);
-        },
-        async click(selector) {
-            if (await this.wait(selector))
-                await page.click(selector);
-        },
-        async focus(selector) {
-            if (await this.wait(selector))
-                await page.focus(selector);
-        },
-        async hover(selector) {
-            if (await this.wait(selector))
-                await page.hover(selector);
-        },
-        async select(selector) {
-            if (await this.wait(selector))
-                await page.select(selector);
-        },
-        async tap(selector) {
-            if (await this.wait(selector))
-                await page.tap(selector);
-        },
-        async wait(selector) {
-            try {
-                await page.waitForSelector(selector);
-                return true;
-            } catch {
-                return false;
-            }
-        },
-        async scroll(selector) {
-            await page.evaluate((selector) => {
-                const el = document.querySelector(selector);
-                el && el.scrollIntoView({ behavior: 'smooth' });
-            }, selector);
-        },
-        async sleep(ms) {
-            ms = parseInt(ms);
-            ms && await Bun.sleep(ms)
-        },
-        async press(key) {
-            await page.keyboard.press(key);
-        },
-        async type(text) {
-            if (await this.wait('*:focus'))
-                await page.type('*:focus', text);
-        },
-        async browser(js) {
-            return await page.evaluate((r) => eval(r), js);
-        },
-        async server(js) {
-            return eval(js);
-        },
-        async save(string) {
-            const [filename, ...data] = string.split(' ');
-            await Bun.write(filename, data.join(' '));
-            return true;
-        },
-        async yesNO() {
-            console.write('[y/N] ');
-            for await (const _ of console)
-                return /y(es)?/.test(_.trim().toLowerCase()) ? 1 : this.exit(1);
-        },
-        async YESno() {
-            console.write('[Y/n] ');
-            for await (const _ of console)
-                return /n(o)?/.test(_.trim().toLowerCase()) ? this.exit(1) : 1;
-        },
-        async log() { }
-    };
-
-    const VAR = {};
-
-    function parse_cmd(line) {
-        let char = '';
-        let i = 0;
-        let cmd = '';
-        let args = '';
-        while ((char = line.charAt(i++)) && char !== ' ')
-            cmd += char;
-        while ((char = line.charAt(i++)))
-            args += char;
-        for (const key in VAR)
-            args = args.replace(key, VAR[key]);
-        return [cmd, args];
-    }
-
-    await parse(file, async (line) => {
-        if (!line || line.startsWith('#'))
-            return;
-
-        await COMMANDS.sleep(150);
-
-        if (line.startsWith('$')) {
-            const [name, _] = parse_cmd(line);
-            const [command, args] = parse_cmd(_);
-            try {
-                VAR[name] = command in COMMANDS
-                    ? await COMMANDS[command](args) : _;
-            }
-            catch { }
-            return;
-        }
-
-        const [command, args] = parse_cmd(line);
-        console.write(esc(command, 38, 5, 244, 1), ' ', args, ' ');
-
-        try {
-            await COMMANDS[command](args);
-            console.write(esc('OK', 38, 5, 155, 1), '\n');
-        }
-        catch (err) {
-            console.write(esc('ERR', 38, 5, 203, 1), '\n');
-            DEBUG && console.log(err);
-        }
-    });
-}
 
 async function parse(str, f) {
     let LF = true;
